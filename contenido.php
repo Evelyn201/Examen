18f5a7ed65db43f8168b0d1c89d630c2895a59a9
Evelyn201 <72232806+Evelyn201@users.noreply.github.com>
2020-10-03 22:58:12 -0500

    Version 1

---
+++ contenido.php
@@ -0,0 +1,25 @@
+<?php 
+
+	function contenido(){
+		$datos=array();
+
+		$datos[0]="img/Azul_.jpg"."||".
+				  "Nerenes:"."||".
+				  "Produce un sentimiento de serenidad y tranquilidad.Transmite, sin lugar a dudas, una personalidad que domina; una confianza en sí mismo y una gran seguridad. El más frío
+				      e inmaterial de los colores. Color del infinito, del cielo y del mar, significa idelidad, justicia, verdad, caridad.";
+
+		$datos[1]="img/Blanca.jpg"."||".
+				  "Ondinas:"."||".
+				  "Este color es la síntesis de todos los colores. En sentido positivo significa perfección, pureza, verdad, inocencia, integridad, firmeza y obediencia. Un color muy utilizado en los hospitales y casas de salud.";
+
+		$datos[2]="img/Roja.jpg"."||".
+				  "Hiarrus:"."||".
+				  "Este es el más cálido de los colores. Es estimulante y dinamiza a quien lo usa. Significa fortaleza, amor, sacrificio, audacia, optimismo y victoria. Pero también sangre, fuego, agresividad, pasiones provocativas. Las personas que visten de rojo, no pueden evitar darse siempre a notar.";
+		$datos[3]="img/Verde.jpg"."||".
+				  "Dama Verde:"."||".
+				  "El verde es el color del equilibrio y funciona con nuestra dualidad. Se encuentra en el centro del espectro y es el color que se enfoca directamente sobre la retina del lente del ojo, brindandole relajacion, que posteriormente se transmite al resto del cuerpo.";
+
+		return $datos;
+	}
+
+ ?>
